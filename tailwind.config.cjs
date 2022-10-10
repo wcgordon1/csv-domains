5b8ea7923c78cc0215b2a93dbb3eb0f36bb2178d
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-10-10 15:22:32 +0300

    added: infopages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,112 +1,160 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontSize: {
       xs: [
-        "0.75rem",
+        '0.75rem',
         {
-          lineHeight: "1rem",
+          lineHeight: '1rem',
         },
       ],
       sm: [
-        "0.875rem",
+        '0.875rem',
         {
-          lineHeight: "1.5rem",
+          lineHeight: '1.5rem',
         },
       ],
       base: [
-        "1rem",
+        '1rem',
         {
-          lineHeight: "1.75rem",
+          lineHeight: '1.75rem',
         },
       ],
       lg: [
-        "1.125rem",
+        '1.125rem',
         {
-          lineHeight: "2rem",
+          lineHeight: '2rem',
         },
       ],
       xl: [
-        "1.25rem",
+        '1.25rem',
         {
-          lineHeight: "2rem",
+          lineHeight: '2rem',
         },
       ],
-      "2xl": [
-        "1.5rem",
+      '2xl': [
+        '1.5rem',
         {
-          lineHeight: "2rem",
+          lineHeight: '2rem',
         },
       ],
-      "3xl": [
-        "2rem",
+      '3xl': [
+        '2rem',
         {
-          lineHeight: "2.5rem",
+          lineHeight: '2.5rem',
         },
       ],
-      "4xl": [
-        "2.5rem",
+      '4xl': [
+        '2.5rem',
         {
-          lineHeight: "3.5rem",
+          lineHeight: '3.5rem',
         },
       ],
-      "5xl": [
-        "3rem",
+      '5xl': [
+        '3rem',
         {
-          lineHeight: "3.5rem",
+          lineHeight: '3.5rem',
         },
       ],
-      "6xl": [
-        "3.75rem",
+      '6xl': [
+        '3.75rem',
         {
-          lineHeight: "1",
+          lineHeight: '1',
         },
       ],
-      "7xl": [
-        "4.5rem",
+      '7xl': [
+        '4.5rem',
         {
-          lineHeight: "1.1",
+          lineHeight: '1.1',
         },
       ],
-      "8xl": [
-        "6rem",
+      '8xl': [
+        '6rem',
         {
-          lineHeight: "1",
+          lineHeight: '1',
         },
       ],
-      "9xl": [
-        "8rem",
+      '9xl': [
+        '8rem',
         {
-          lineHeight: "1",
+          lineHeight: '1',
         },
       ],
     },
     extend: {
       backgroundImage: (theme) => ({
-        screens: "url('/path-to/image.png')",
-      }),
+        limeToSky: "linear-gradient(100grad,#df3,#aafc62,#83fb26,#46f558,#32ebd3,#33dbe3);",
+        limeToMint: "linear-gradient(100grad,#df3,#aafc62);",
+        desktopGradient: "linear-gradient(100grad,#83fb26,#46f558);",
+        desktopGradient: "linear-gradient(100grad,#32ebd3,#33dbe3)",
+        mike: "url('../images/mike.jpg')",
 
+      }),
+      textShadow: {
+        largeShadow: '0 192px 136px rgba(26,43,59,0.23),0 70px 50px rgba(26,43,59,0.16),0 34px 24px rgba(26,43,59,0.13),0 17px 12px rgba(26,43,59,0.1),0 7px 5px rgba(26,43,59,0.07);',
+      },
+      boxShadow: {
+        DEFAULT: '0 2px 6px -1px rgba(0, 0, 0, 0.16), 0 1px 4px -1px rgba(0, 0, 0, 0.04);',
+        active: '0 0 8px -2px rgba(0, 0, 0, 0.1), 0 6px 20px -3px rgba(0, 0, 0, 0.2);',
+        card: '0 50px 60px rgb(12 25 39 / 10%), 0 16px 20px rgb(12 25 39 / 6%), 0 6px 8px rgb(12 25 39 / 5%)',
+        largeShadow: '0 192px 136px rgba(26,43,59,0.23),0 70px 50px rgba(26,43,59,0.16),0 34px 24px rgba(26,43,59,0.13),0 17px 12px rgba(26,43,59,0.1),0 7px 5px rgba(26,43,59,0.07);',
+      },
       colors: {
-        "accent": {
-          "50": "#E5E5FF",
-          "100": "#CCCCFF",
-          "200": "#9999FF",
-          "300": "#6666FF",
-          "400": "#3333FF",
-          "500": "#0000FF",
-          "600": "#0000CC",
-          "700": "#000099",
-          "800": "#000066",
-          "900": "#000033"
+        black: '#111213',
+        tangaroa: '#1a2b3b',
+        mint: '#31f2cc',
+        lime: '#df3',
+        orange: '#fa6d3e',
+        blue: '#3bf',
+        azure: '#4E81B1',
+
+        mercury: '#e1e5e9',
+        gray: '#667380',
+
+        tangaroa: {
+          '50': '#E3EBF3',
+          '100': '#C3D4E4',
+          '200': '#8AACCC',
+          '300': '#4E81B1',
+          '400': '#335575',
+          '500': '#1A2B3B',
+          '600': '#14222E',
+          '700': '#101A23',
+          '800': '#0B1219',
+          '900': '#05080B',
+        },
+        "mercury": {
+          "50": "#f5f7f9",
+          "100": "#f8f9fc",
+          "200": "#f1f3f9",
+          "300": "#dee3ed",
+          "400": "#c2c9d6",
+          "500": "#8f96a3",
+          "600": "#5e636e",
+          "700": "#2f3237",
+          "800": "#1d1e20",
+          "900": "#111213"
         },
       },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '3rem',
+        '6xl': '5rem',
+      },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Inter Tight', ...defaultTheme.fontFamily.sans],
+        mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
       },
     },
   },
-  plugins: [],
+  plugins: [
+     require('tailwind-scrollbar-hide'),
+    require('tailwindcss-textshadow'),
+    require('@tailwindcss/typography'),
+    // ...
+  ],
 }
