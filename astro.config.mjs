698729f24bb84ea75f67c9936ef3d3a4ab09acc5
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-10-19 08:21:32 +0300

    added: landing intro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
    site: 'https://supremeuikits.com',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+  integrations: [tailwind(), image({
+    // By default, transformed images will be cached to ./node_modules/.astro/image. This can be configured in the integration’s config options. may be useful if your hosting provider allows caching between CI builds
+    cacheDir: "./.cache/image"
+  }), compress(), partytown(), sitemap()]
 });
