229f5fe39fb687338218cd6210acb4e66c4901b2
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-11-13 08:13:08 +0200

    added sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
    site: 'https://supremeuikits.com',
   integrations: [tailwind(), image({
-    // By default, transformed images will be cached to ./node_modules/.astro/image. This can be configured in the integration’s config options. may be useful if your hosting provider allows caching between CI builds
-    cacheDir: "./.cache/image"
+    serviceEntryPoint: '@astrojs/image/sharp'
   }), compress(), partytown(), sitemap()]
 });
