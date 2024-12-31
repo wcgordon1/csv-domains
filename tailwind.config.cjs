35d0ebec538c4842d0e25226e6d61a208e040cec
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-12-31 23:16:14 +0200

    upgraded to Tailwind V4 & Astro V5

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,18 +6,7 @@ module.exports = {
 
     extend: {
       colors: {
-        purple: {
-          50: "#E6E7F4",
-      100: "#D1D3EB",
-      200: "#9FA4D5",
-      300: "#7178C1",
-      400: "#474FA3",
-      500: "#333875",
-      600: "#282D5C",
-      700: "#1F2247",
-      800: "#14162E",
-      900: "#0B0C19"
-        },
+       
         blue: {
          50: "#EBF2FE",
       100: "#D7E4FE",
@@ -36,12 +25,7 @@ module.exports = {
         '5xl': '3rem',
         '6xl': '5rem',
       },
-      fontFamily: {
-        display: ['Cabinet Grotesk', ...defaultTheme.fontFamily.sans],
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
-
-      },
+     
     },
   },
   plugins: [
